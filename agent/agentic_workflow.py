0f0e23c3952c8f0e08ce41752e5931e93cd6afd1
rbose4 <roopbose9@gmail.com>
2025-08-20 12:45:12 -0400

    class and import statements added

---
+++ agent/agentic_workflow.py
@@ -0,0 +1,18 @@
+from utils.model_loader import ModelLoader
+from prompt_library.prompt import SYSTEM_PROMPT
+from langgraph.graph import StateGraph, MessagesState, START, END
+from langgraph.prebuilt import ToolNode, tools_condition
+
+
+class GraphBuilder():
+    def __init__(self):
+        pass
+    
+    def agent_function(self):
+        pass
+    
+    def build_graph(self):
+        pass
+    
+    def __call__(self):
+        pass
